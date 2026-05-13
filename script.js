86f4c8bf56ef4cbdb121a776157d437a91f7d5dc
mrpelzer <mr.pelzer@aoiths.org>
2026-05-13 12:30:52 -0400

    Create Pet class with name and breed properties
    Added a Pet class with properties and a method to display pet information.

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 /* === EXERCISE 1 CODE ALONG === */
 
-
+class Pet {
+  constructor(name, breed) {
+      this.name = name;
+      this.breed = breed;
+      this.show = function() {
+          var text = "<p>Your pet's name is " + this.name + ". The pet's breed is " + this.breed + ".</p>";
+          return text;
+      };
+  }
+}
 
 
 
